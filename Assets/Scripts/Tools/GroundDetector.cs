2a3cdd862ce001224806c60f32766267248dd37e
wagskydive <wagskydive@gmail.com>
2022-10-28 03:24:13 +0200

    Start On RaycastLogic

---
+++ Assets/Scripts/Tools/GroundDetector.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+
+public class GroundDetector : MonoBehaviour
+{
+    bool isOnGround;
+
+    public bool IsOnGround{get =>isOnGround;}
+
+    Transform RaycastStart;
+    float TerrainDistance()
+    {
+        return 0;
+    }
+
+}
+
---
+++ Assets/Tests/PlayModeTests/RayCastTests.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+
+public class RayCastTests
+{
+    // [UnityTest]
+    // public IEnumerator RaycastGroundDetectAfterJump()
+    // {
+    //     var gameObject = new GameObject();
+    //     var characterMovementController = gameObject.AddComponent<CharacterMovementController>();
+    //     var characterAnimationController = gameObject.AddComponent<CharacterAnimationController>();
+    //     var groundDetector = gameObject.AddComponent<GroundDetector>();
+    //     var inputFaker = gameObject.AddComponent<ContiniousInputFaker>();
+
+    //     characterMovementController.AssignInput(inputFaker);
+    //     inputFaker.TestInputJump();
+
+    //     yield return new WaitForSeconds(2f);
+        
+    //     Assert.IsTrue(groundDetector.IsOnGround);
+           
+    // }
+}
